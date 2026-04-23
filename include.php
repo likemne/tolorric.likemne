d327bf9418a50704260c81cdb4e2ba64fe74a413
Evgeniy <51183910+likemne@users.noreply.github.com>
2026-04-23 15:50:42 +0700

    Update include.php

--- include.php
+++ include.php
@@ -1,16 +1,11 @@
 <?php
-// /local/modules/tolorric.likemne/include.php
 
 use Bitrix\Main\Loader;
 
 Loader::registerAutoLoadClasses(
     'tolorric.likemne',
     [
-        // Здесь будет список "Класс" => "путь/к/файлу.php"
+        // Здесь будут классы твоего модуля:
         // 'Tolorric\Likemne\SomeClass' => 'lib/someclass.php',
     ]
 );
-
-// Здесь же можно будет регистрировать обработчики событий
-// $eventManager = \Bitrix\Main\EventManager::getInstance();
-// $eventManager->addEventHandler('main', 'OnPageStart', ['MyClass', 'myMethod']);
