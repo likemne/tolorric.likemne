13731ea7852a71a084cb2d7199ded654a432605b
Evgeniy <51183910+likemne@users.noreply.github.com>
2026-04-23 15:45:20 +0700

    Add files via upload

---
+++ lang/ru/index.php
@@ -0,0 +1,5 @@
+<?php
+$MESS["TOLORRIC_MODULE_NAME"] = "Мой первый модуль Tolorric";
+$MESS["TOLORRIC_MODULE_DESCRIPTION"] = "Мастер-модуль, который учится стандартам Битрикс";
+$MESS["TOLORRIC_PARTNER_NAME"] = "Моё Имя";
+$MESS["TOLORRIC_PARTNER_URI"] = "https://example.com";
