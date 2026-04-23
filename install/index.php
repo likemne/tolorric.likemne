33a383f26db7b4acc9f35468ee03d7f41027881c
Evgeniy <51183910+likemne@users.noreply.github.com>
2026-04-23 15:56:32 +0700

    Update index.php

--- install/index.php
+++ install/index.php
@@ -1,54 +1,30 @@
-<?php
-
-use Bitrix\Main\Localization\Loc;
-use Bitrix\Main\ModuleManager;
-
-Loc::loadMessages(__FILE__);
-
-class tolorric_likemne extends CModule
+public function DoInstall()
 {
-    public $MODULE_ID = "tolorric.likemne";
-    public $MODULE_VERSION;
-    public $MODULE_VERSION_DATE;
-    public $MODULE_NAME;
-    public $MODULE_DESCRIPTION;
-    public $PARTNER_NAME;
-    public $PARTNER_URI;
-
-    public $errors = [];
-
-    public function __construct()
-    {
-        $arModuleVersion = [];
-        include(__DIR__ . "/version.php");
-
-        $this->MODULE_VERSION = $arModuleVersion["VERSION"];
-        $this->MODULE_VERSION_DATE = $arModuleVersion["VERSION_DATE"];
-        $this->MODULE_NAME = Loc::getMessage("TOLORRIC_MODULE_NAME");
-        $this->MODULE_DESCRIPTION = Loc::getMessage("TOLORRIC_MODULE_DESCRIPTION");
-        $this->PARTNER_NAME = Loc::getMessage("TOLORRIC_PARTNER_NAME");
-        $this->PARTNER_URI = Loc::getMessage("TOLORRIC_PARTNER_URI");
+    if ($this->isAlreadyInstalled()) {
+        $this->errors[] = Loc::getMessage("TOLORRIC_MODULE_ALREADY_INSTALLED");
+        return false;
     }
+    
+    ModuleManager::registerModule($this->MODULE_ID);
+    
+    // Создаём таблицу в БД
+    $this->installDB();
+    
+    return true;
+}
 
-    public function DoInstall()
-    {
-        if ($this->isAlreadyInstalled()) {
-            $this->errors[] = Loc::getMessage("TOLORRIC_MODULE_ALREADY_INSTALLED");
-            return false;
-        }
-        
-        ModuleManager::registerModule($this->MODULE_ID);
-        return true;
+public function installDB()
+{
+    $connection = \Bitrix\Main\Application::getConnection();
+    if (!$connection->isTableExists(\Tolorric\Likemne\NoteTable::getTableName())) {
+        \Tolorric\Likemne\NoteTable::getEntity()->createDbTable();
     }
+}
 
-    public function DoUninstall()
-    {
-        ModuleManager::unRegisterModule($this->MODULE_ID);
-        return true;
-    }
-
-    private function isAlreadyInstalled(): bool
-    {
-        return ModuleManager::isModuleInstalled($this->MODULE_ID);
+public function uninstallDB()
+{
+    $connection = \Bitrix\Main\Application::getConnection();
+    if ($connection->isTableExists(\Tolorric\Likemne\NoteTable::getTableName())) {
+        $connection->dropTable(\Tolorric\Likemne\NoteTable::getTableName());
     }
 }
