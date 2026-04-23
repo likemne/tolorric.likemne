6068aeb6daf29ac9dc19ee83b7f80c003444d5c8
Evgeniy <51183910+likemne@users.noreply.github.com>
2026-04-23 15:50:15 +0700

    Update index.php

--- lang/ru/index.php
+++ lang/ru/index.php
@@ -3,3 +3,4 @@ $MESS["TOLORRIC_MODULE_NAME"] = "Мой первый модуль Tolorric";
 $MESS["TOLORRIC_MODULE_DESCRIPTION"] = "Мастер-модуль, который учится стандартам Битрикс";
 $MESS["TOLORRIC_PARTNER_NAME"] = "Моё Имя";
 $MESS["TOLORRIC_PARTNER_URI"] = "https://example.com";
+$MESS["TOLORRIC_MODULE_ALREADY_INSTALLED"] = "Модуль уже установлен";
